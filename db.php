0dc20ca7c7817467ec45b076ea3c7c3836bf99c9
yusron <yusron.dev@gmail.com>
2024-10-29 09:53:28 +0700

    fix

--- db.php
+++ db.php
@@ -1,11 +1,16 @@
-<?php
-$host = 'localhost';
-$user = 'root';
-$password = '';
-$database = 'test_db';
+<?php 
 
-$conn = new mysqli($host, $user, $password, $database);
+$host = $_ENV['PG_HOST'];
+$port = $_ENV['PG_PORT'];
+$db = $_ENV['PG_DB'];
+$user = $_ENV['PG_USER'];
+$password = $_ENV['PG_PASSWORD'];
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+$connection_string = "host=" . $host . " port=" . $port . " dbname=" . $db . " user=" . $user . " password=" . $password . " sslmode=require";
+
+$dbconn = pg_connect($connection_string);
+
+if (!$dbconn) {
+    die("Connection failed: " . pg_last_error());
 }
+echo "Connected successfully";
